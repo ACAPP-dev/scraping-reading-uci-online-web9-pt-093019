76f448e5a02772898a3f6ed39e70fbf768df0a3b
Andrew Capp <acapp909@gmail.com>
2019-11-13 03:12:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,6 +8,6 @@ doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
 p doc.css(".headline-26OIBN").text
 
-p courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b.inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC").text
+p courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC").text
 
 #binding.pry
